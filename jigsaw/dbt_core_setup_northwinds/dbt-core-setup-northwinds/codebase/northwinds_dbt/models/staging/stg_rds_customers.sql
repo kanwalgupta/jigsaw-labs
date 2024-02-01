39d15c400412c2086c87e051e332556fcf291ae7
eojhom <eojhom@gmail.com>
2024-01-31 20:15:31 -0500

    build stg_rds_customers

---
+++ jigsaw/dbt_core_setup_northwinds/dbt-core-setup-northwinds/codebase/northwinds_dbt/models/staging/stg_rds_customers.sql
@@ -0,0 +1,13 @@
+WITH source AS (
+    SELECT * 
+    FROM public.customers
+), -- import CTE
+renamed AS (
+    SELECT 
+      customer_id,
+      country,
+      split_part(contact_name, ' ', 1) AS first_name,
+      split_part(contact_name, ' ', 2) AS last_name
+    FROM source
+) -- logical CTE
+SELECT * FROM renamed --final select
